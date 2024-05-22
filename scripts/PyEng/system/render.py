bff942d5af3a2c73025d52842c0072f4dccaeaf7
gustavo <gustavo@phntms.com>
2024-05-22 11:50:23 +0100

    Add docs

--- scripts/PyEng/system/render.py
+++ scripts/PyEng/system/render.py
@@ -1,30 +1,35 @@
 import pygame
 from ..element_manager.components import SystemComponents
 from typing import Literal
-
+from ..types import Coordinate
 
 class Render(SystemComponents):
     """
     Render all the elements in the Window
     """
-    def __init__(self, game_type: Literal["default", "isometric"]):
+    def __init__(self) -> None:
         SystemComponents.__init__(self)
-        self.game_type = game_type
         self.render_group = []
 
-    def add_to_render_group(self, img, pos, dest_surf, layer=0):
-        if self.game_type == "isometric":
-            self.render_group.append((img, pos, layer, dest_surf))
-        else:
-            # sort the list by the layers. layers is what goes on top of each other, 0 is the most visible layer and positive layer is blit behind
-            self.render_group.append((img, pos, layer, dest_surf))
-            self.render_group.sort(key=lambda x: x[3], reverse=True)
+    def add_to_render_group(self, img: pygame.Surface, pos: Coordinate,
+                            dest_surf: pygame.Surface, layer: int) -> None:
+        """Function to add elements to be rendered into a queue
+
+        Args:
+            img (pygame.Surface): The image/Surface that will be rendered
+            pos (Coordinate): The position to render (x, y)
+            dest_surf (pygame.Surface): The destination surface to render to (display, ui...)
+            layer (int, optional): The layer (order) to render to.
+        """
+        self.render_group.append((img, pos, dest_surf, layer))
+   
 
     def render(self):
+        """Render every element in the render group to its respective destination surface"""
         for group in self.render_group:
-            group[2].blit(group[0], group[1])
+            dest_surf: pygame.Surface = group[2]
+            dest_surf.blit(group[0], group[1])
 
     def update(self):
         self.render()
-
 
