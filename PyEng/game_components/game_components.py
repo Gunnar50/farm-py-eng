ae7336393db8a8fce924d4707224a00dde8bacad
Gustavo <gustavo.passarella@hotmail.com>
2024-09-05 21:55:01 +0100

    Update basemodel

--- PyEng/game_components/game_components.py
+++ PyEng/game_components/game_components.py
@@ -1,15 +1,32 @@
 from dataclasses import dataclass
+from typing import Optional
 import pygame
 from PyEng.element_manager import components
 
 
 @dataclass
 class BaseModel(components.GameComponent):
+  '''
+  Type: class that belongs to
+  Group: the group type (eg. tiles, crops)
+  Layer: the layer that is rendered in (crops are rendered on top of tiles)
+  
+  '''
   label: str
   group: str
   layer: int
-  image_path: str
-  image_surface: list[pygame.Surface]
+  image_path: list[str]
+  image: Optional[list[pygame.Surface]] = None
+
+  # Executes right after the __init__ method
+  def __post_init__(self):
+    # Generate image
+    if self.image is None:
+      # TODO - update tile size
+      self.image = [
+          pygame.transform.scale(pygame.image.load(image), (100, 100))
+          for image in self.image_path
+      ]
 
 
 @dataclass
