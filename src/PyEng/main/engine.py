5854805fa9055a748ad32dc98f6481fae5294998
Gustavo <gustavo.passarella@hotmail.com>
2024-09-26 17:41:25 +0100

    Update engine

--- src/PyEng/main/engine.py
+++ src/PyEng/main/engine.py
@@ -1,7 +1,7 @@
 import os
 
 from src.PyEng.components.components import ComponentManager
-from src.PyEng.components.input import Keyboard, Mouse
+from src.PyEng.components.input import Input
 from src.PyEng.components.window import Window
 from src.PyEng.main.engine_config import EngineConfigs
 from src.PyEng.main.engine_files import EngineFiles
@@ -12,20 +12,62 @@ from src.shared.debug import LOGGER
 
 
 class Engine:
-
   __instance = None
 
-  def __init__(
-      self,
-      window: Window,
-      mouse: Mouse,
-      keyboard: Keyboard,
-  ):
+  def __init__(self, configs: EngineConfigs):
+    self.window: Window
+    self.input: Input
+
+    self.check_assets_folder()
+    self.create_global_components(configs)
+    self.create_engine_components(configs)
+
     self.components_manager = ComponentManager()
-    self.window = window
-    self.mouse = mouse
-    self.keyboard = keyboard
     self.close_flag = False
+
+    Engine.__instance = self
+
+  @classmethod
+  def get_instance(cls):
+    if cls.__instance is None:
+      cls.create()
+    return cls.__instance
+
+  def check_assets_folder(self):
+    if not os.path.exists(EngineFiles.ASSETS_FOLDER):
+      raise exceptions.IllegalStateException(
+          f"Can't create engine - assets folder not found in root directory: {EngineFiles.ROOT_FOLDER}."
+      )
+
+  def create_global_components(self, configs: EngineConfigs):
+    # Create global systems
+    ErrorManager(EngineFiles.ERROR_FOLDER)
+    Debugger(configs.debugger)
+    # BackgroundLoader()
+
+  def create_engine_components(self, configs: EngineConfigs):
+    # Create engine components
+    self.window = Window(
+        window_width=configs.window_width,
+        window_height=configs.window_height,
+        fullscreen=configs.fullscreen,
+        caption=configs.caption,
+        fps=configs.fps,
+        vsync=configs.vsync,
+        background_colour=configs.background_colour,
+    )
+    # Set up keyboard and mouse inputs
+    self.input = Input(EngineFiles.KEY_MAPPINGS)
+    # self.timer = FrameTimer(configs.fps)
+    # self.state_manager = StateManager()
+
+    # Create UI
+    # UserInterface(
+    #     self.window,
+    #     self.mouse,
+    #     self.keyboard,
+    #     configs.ui_configs,
+    # )
 
   def update(self) -> None:
     self.components_manager.update()
@@ -50,51 +92,4 @@ class Engine:
       raise exceptions.IllegalStateException(
           'Engine has already been initialised!')
 
-    return EngineCreator(configs).get_instance()
-
-
-class EngineCreator:
-
-  def __init__(self, configs: EngineConfigs):
-    if not os.path.exists(EngineFiles.ASSETS_FOLDER):
-      raise exceptions.IllegalStateException(
-          f"Can't init engine - assets folder not found in root directory {EngineFiles.ROOT_FOLDER}."
-      )
-
-    # Create gloabal systems
-    ErrorManager(EngineFiles.ERROR_FOLDER)
-    Debugger(configs.debugger)
-    # BackgroundLoader()
-
-    # Create engine instance
-    self.engine = self.create_engine_instance(configs)
-
-  def get_instance(self) -> 'Engine':
-    return self.engine
-
-  def create_engine_instance(self, configs: EngineConfigs):
-    window = Window(
-        window_width=configs.window_width,
-        window_height=configs.window_height,
-        fullscreen=configs.fullscreen,
-        caption=configs.caption,
-        fps=configs.fps,
-        vsync=configs.vsync,
-        background_colour=configs.background_colour,
-    )
-    keyboard = Keyboard()
-    mouse = Mouse()
-    # timer = FrameTimer(configs.fps)
-    # state_manager = StateManager()
-    # Create UI
-    # UserInterface(
-    #     window,
-    #     mouse,
-    #     keyboard,
-    #     configs.ui_configs,
-    # )
-    return Engine(
-        window,
-        mouse,
-        keyboard,
-    )
+    return cls(configs)
