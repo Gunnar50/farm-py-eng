3a08027d87aa86797424ab36210a604d508d8036
Gustavo <gustavo.passarella@hotmail.com>
2024-09-26 17:41:32 +0100

    Add tests

---
+++ src/PyEng/tests/conftest.py
@@ -0,0 +1,6 @@
+import sys
+import os
+
+# Add the root of the project to sys.path
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
---
+++ src/PyEng/tests/engine_test.py
@@ -0,0 +1,40 @@
+from unittest import mock
+import pytest
+
+from src.PyEng.main.engine_config import EngineConfigs
+from src.shared import exceptions
+from src.PyEng.main.engine import Engine
+
+
+@pytest.fixture(autouse=True)
+def mock_window():
+  with mock.patch('src.PyEng.main.engine.Window', autospec=True) as mock_window:
+    yield mock_window
+
+
+@pytest.fixture(autouse=True)
+def reset_engine_singleton():
+  Engine._Engine__instance = None
+
+
+class TestEngine:
+  configs = EngineConfigs.get_default_configs()
+
+  def test_singleton_behavior(self):
+    engine = Engine.create()
+    assert isinstance(engine, Engine)
+
+    with pytest.raises(exceptions.IllegalStateException):
+      Engine.create()
+
+  def test_get_engine_instance(self):
+    engine = Engine.create()
+    engine_instance = Engine.get_instance()
+    assert engine == engine_instance
+
+  @mock.patch('os.path.exists')
+  def test_missing_assets_folder(self, mock_exists: mock.Mock):
+    # Patch os.path.exists to return False
+    mock_exists.return_value = False
+    with pytest.raises(exceptions.IllegalStateException):
+      Engine.create()
