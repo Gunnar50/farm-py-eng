4ec5d704c41799cee19247f7c536b99d677cd7f8
gustavo <gustavo.passarella@hotmail.com>
2024-10-09 12:09:12 +0100

    Add todo to game manage

--- src/FarmGame/main/game_manager.py
+++ src/FarmGame/main/game_manager.py
@@ -1,7 +1,7 @@
 from src.FarmGame.main.configs.game_config import GameConfig
 # from src.FarmGame.main.game import Game
 from src.FarmGame.main.game_state import GameState
-from src.FarmGame.repository.game_repository import GameRepository
+from src.FarmGame.repository.game_database import GameDatabase
 # from src.FarmGame.scene.render import Render
 from src.PyEng.main.engine import Engine
 
@@ -12,15 +12,20 @@ class GameManager:
     self.configs = game_configs
     self.engine = Engine.create(game_configs)
 
+    self.game_repository = GameDatabase()
+    # init game controls
+    # init sounds
+    # init session manager
     # self.renderer = Render()
     # self.current_game = Game(self.renderer)
-    self.game_repository = GameRepository()
 
     for i in self.game_repository.entity_system.blueprints:
       print(i.data)
 
-  def update(self):
-    # If there is a session, render the scene and the state is not main_menu
+  def update(self) -> None:
+    # If there is a session (game is running),
+    # and the state is not main_menu
+    # render the scene
     # if self.engine.state_manager.is_not_state(GameState.MAIN_MENU,
     #                                           GameState.INITIAL):
     #   pass
