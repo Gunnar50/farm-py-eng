e3f50a042ab42ff1adc002708cd830c13817fbbf
gustavo <gustavo.passarella@hotmail.com>
2025-06-18 16:23:02 +0100

    Update exceptions

--- src/shared/exceptions.py
+++ src/shared/exceptions.py
@@ -1,30 +1,53 @@
-class ComponentDuplicateError(Exception):
+from typing import Any, Optional
+
+
+class ApplicationException(Exception):
+  """Base class for all application exceptions."""
+
+  def __init__(self, message: str, context: Optional[dict[str, Any]] = None):
+    super().__init__(message)
+    self.context = context
+
+
+class ComponentDuplicateError(ApplicationException):
   '''Component duplicate error exception'''
 
 
-class ComponentNotFoundError(Exception):
+class ComponentNotFoundError(ApplicationException):
   '''Component does not exist or was not found exception'''
 
 
-class InvalidParameters(Exception):
+class InvalidParameters(ApplicationException):
   '''Raise when parameters are invilid'''
 
 
-class FilePathNotFound(Exception):
+class FilePathNotFound(ApplicationException):
   '''Raise if a file path is not found'''
 
 
-class IllegalStateException(Exception):
+class IllegalStateException(ApplicationException):
   '''Raised when a component has an illegal state eg. not being initialised'''
 
 
-class IllegalRegistryOverwrite(Exception):
+class IllegalRegistryOverwrite(ApplicationException):
   '''Raised when a registry is being overwritten'''
 
 
-class RegistryNotFoundException(Exception):
+class RegistryNotFoundException(ApplicationException):
   '''Raised when a registry is not found'''
 
 
-class InfoFileNotFound(Exception):
+class InfoFileNotFound(ApplicationException):
   '''Raised if a entity info file does not exist'''
+
+
+class FailedToLoadBlueprint(ApplicationException):
+  '''Raise if a blueprint fails to load'''
+
+
+class FailedToGetDataModel(ApplicationException):
+  '''Raise when data fails to load in specified model'''
+
+
+class NotDataclass(ApplicationException):
+  '''Raise when the specified class is not a dataclass'''
