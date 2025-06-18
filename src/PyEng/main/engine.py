68c411e72943b1b2653df2f48b8298813980698c
gustavo <gustavo.passarella@hotmail.com>
2025-06-18 16:10:24 +0100

    Add non implemented raise

--- src/PyEng/main/engine.py
+++ src/PyEng/main/engine.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 from src.PyEng.components.components import ComponentManager
 from src.PyEng.components.input import Input
@@ -13,7 +14,7 @@ from src.shared.debug import LOGGER
 
 
 class Engine:
-  __instance = None
+  __instance: Optional['Engine'] = None
 
   def __init__(self, configs: type[EngineConfigs]):
     self.window: Window
@@ -38,7 +39,8 @@ class Engine:
     self.components_manager.update()
 
   def render(self) -> None:
-    self.renderer.render()
+    raise NotImplementedError
+    # self.renderer.render()
 
   def check_assets_folder(self):
     if not os.path.exists(EngineFiles.ASSETS_FOLDER):
@@ -78,15 +80,18 @@ class Engine:
     # )
 
   def get_delta(self) -> float:
-    return self.timer.get_delta()
+    raise NotImplementedError
+    # return self.timer.get_delta()
 
   def get_current_time(self) -> float:
-    return self.timer.get_time()
+    raise NotImplementedError
+    # return self.timer.get_time()
 
   def close_engine(self) -> None:
+    raise NotImplementedError
     # BackgroundLoader.clean_up()
     # Ui.clean_up()
-    self.window.destroy()
+    # self.window.destroy()
 
   @classmethod
   def create(
