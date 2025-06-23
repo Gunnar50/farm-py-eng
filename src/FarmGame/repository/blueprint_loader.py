4fdc731d7f9aed2e4692ec62997b7bff0551abad
gustavo <gustavo.passarella@hotmail.com>
2025-06-23 14:26:56 +0100

    Update import

--- src/FarmGame/repository/blueprint_loader.py
+++ src/FarmGame/repository/blueprint_loader.py
@@ -2,10 +2,7 @@ import dataclasses
 import pathlib
 from typing import Any, Type
 
-from src.FarmGame.repository.game_components import (Blueprint,
-                                                     EntityBlueprint,
-                                                     ItemBlueprint,
-                                                     TileBlueprint)
+from src.FarmGame.repository import game_components as gc
 from src.FarmGame.repository.game_files import GameFiles
 from src.shared import exceptions, io
 from src.shared.hash_registry import HashRegistry
@@ -14,7 +11,7 @@ from src.shared.hash_registry import HashRegistry
 @dataclasses.dataclass
 class BlueprintLoader:
   file_prefix: str
-  blueprint_type: Type[Blueprint]
+  blueprint_type: Type[gc.Blueprint]
   folder: pathlib.Path
 
   def load_folder(self, folder: pathlib.Path,
@@ -31,14 +28,14 @@ class BlueprintLoader:
         self.load_folder(item, repository)
 
   def load_file(self, folder: pathlib.Path,
-                repository: HashRegistry[Blueprint]) -> None:
+                repository: HashRegistry[gc.Blueprint]) -> None:
     info_json_file = self.get_info_file(folder)
     blueprint = io.get_data_model(self.blueprint_type, info_json_file)
     repository.register(blueprint)
 
   def is_folder(self, folder: pathlib.Path) -> bool:
     # Check if any file in the folder ends with .json
-    return any(file.is_file() and file.name.endswith(".json")
+    return any(file.is_file() and file.name.endswith('.json')
                for file in folder.iterdir())
 
   def get_info_file(self, folder: pathlib.Path) -> pathlib.Path:
@@ -52,12 +49,12 @@ class BlueprintLoader:
 @dataclasses.dataclass
 class EntityLoader(BlueprintLoader):
   file_prefix = 'entity_info'
-  blueprint_type = EntityBlueprint
+  blueprint_type = gc.EntityBlueprint
   folder = GameFiles.get_entities_folder()
 
   @classmethod
-  def load(cls) -> HashRegistry[EntityBlueprint]:
-    registry: HashRegistry[EntityBlueprint] = HashRegistry(
+  def load(cls) -> HashRegistry[gc.EntityBlueprint]:
+    registry: HashRegistry[gc.EntityBlueprint] = HashRegistry(
         registry_name='entities')
     loader = cls(cls.file_prefix, cls.blueprint_type, cls.folder)
     loader.load_folder(loader.folder, registry)
@@ -67,12 +64,13 @@ class EntityLoader(BlueprintLoader):
 @dataclasses.dataclass
 class TilesLoader(BlueprintLoader):
   file_prefix = 'tile_info'
-  blueprint_type = TileBlueprint
+  blueprint_type = gc.TileBlueprint
   folder = GameFiles.get_tiles_folder()
 
   @classmethod
-  def load(cls) -> HashRegistry[TileBlueprint]:
-    registry: HashRegistry[TileBlueprint] = HashRegistry(registry_name='tiles')
+  def load(cls) -> HashRegistry[gc.TileBlueprint]:
+    registry: HashRegistry[gc.TileBlueprint] = HashRegistry(
+        registry_name='tiles')
     loader = cls(cls.file_prefix, cls.blueprint_type, cls.folder)
     loader.load_folder(loader.folder, registry)
     return registry
@@ -81,12 +79,13 @@ class TilesLoader(BlueprintLoader):
 @dataclasses.dataclass
 class ItemLoader(BlueprintLoader):
   file_prefix = 'item_info'
-  blueprint_type = ItemBlueprint
+  blueprint_type = gc.ItemBlueprint
   folder = GameFiles.get_items_folder()
 
   @classmethod
-  def load(cls) -> HashRegistry[ItemBlueprint]:
-    registry: HashRegistry[ItemBlueprint] = HashRegistry(registry_name='items')
+  def load(cls) -> HashRegistry[gc.ItemBlueprint]:
+    registry: HashRegistry[gc.ItemBlueprint] = HashRegistry(
+        registry_name='items')
     loader = cls(cls.file_prefix, cls.blueprint_type, cls.folder)
     loader.load_folder(loader.folder, registry)
     return registry
